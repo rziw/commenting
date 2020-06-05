1f0ea7088b2b943ca36a70966ecb64e02c1564bd
RZIW <mrs.safari20@gmail.com>
2020-06-05 12:54:12 +0430

    add a post

---
+++ tests/Feature/PostTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
+use Tests\TestCase;
+
+class PostTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function user_can_create_a_post()
+    {
+        $attributes = [
+            'title'                 => 'test post',
+            'description'           => 'example test fake',
+        ];
+
+        $response = $this->post('/posts', $attributes);
+
+        $this->assertDatabaseHas('posts', $attributes);
+    }
+}
