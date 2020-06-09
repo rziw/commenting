e2c93149e779aff41b78c6c995627cb515069c10
RZIW <mrs.safari20@gmail.com>
2020-06-09 16:04:44 +0430

    show comments of the post to pass the comment tests

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -2,3 +2,10 @@
     <h1>{{$post->title}}</h1>
     <p>{{$post->description}}</p>
 </div>
+<div>
+    @foreach($post->comments as $comment)
+        <div>
+            {{$comment->description}}
+        </div>
+    @endforeach
+</div>
